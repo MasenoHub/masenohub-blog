ea3cc61b0dd7e6a31e2a9c318f1ab30e5f727804
sixpeteunder <plenjo15@gmail.com>
2020-03-27 12:31:40 +0300

    Add duty, soft deletes to User model

--- app/User.php
+++ app/User.php
@@ -5,10 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends \TCG\Voyager\Models\User
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
