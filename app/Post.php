e4a904866090d9bb9fb4866773f034a1a9a1df42
sixpeteunder <plenjo15@gmail.com>
2020-03-29 09:31:29 +0300

    Set up laravel-tags

--- app/Post.php
+++ app/Post.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Tags\HasTags;
 
 class Post extends Model
 {
     use SoftDeletes;
+    use HasTags;
 
     /**
      * The attributes that are mass assignable.
